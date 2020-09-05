c8243131c9b33c3447f89b974ebef441190029be
unknown <asifuddaulaasif@gmail.com>
2020-09-05 16:53:41 +0600

    blog list added

---
+++ resources/views/blog_list.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Blog list</title>
+</head>
+<body>
+<div class="blog-list">
+    <ul>
+        <li>Blog 1</li>
+        <li>Blog 2</li>
+        <li>Blog 3</li>
+    </ul>
+</div>
+</body>
+</html>
